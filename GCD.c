462ce0095d622ef0738fd4a3c3babe6e79ed1fb3
Weeb-C <udbhavpurwar119@gmail.com>
2025-02-06 14:48:49 +0530

    First Commit

--- GCD.c
+++ GCD.c
@@ -16,3 +16,20 @@ int main() {
     printf("%d\n", a);
     return 0;
 }
+/*
+ * This program calculates the Greatest Common Divisor (GCD) of two integers
+ * using the Euclidean algorithm.
+ *
+ * Steps:
+ * 1. Prompt the user to enter two integers.
+ * 2. Read the two integers using scanf. If the input is invalid, display an error
+ *    message and terminate the program.
+ * 3. Display the two integers for which the GCD will be calculated.
+ * 4. Use a while loop to apply the Euclidean algorithm:
+ *    - Continue looping until the second integer (b) becomes 0.
+ *    - In each iteration, calculate the remainder (t) of a divided by b.
+ *    - Update a to b and b to the remainder (t).
+ * 5. Once the loop ends, the variable 'a' contains the GCD.
+ * 6. Print the computed GCD.
+ */
+
--- longest-contigues-subsequence.c
+++ longest-contigues-subsequence.c
@@ -1,34 +1,23 @@
-#include <stdio.h>
-
-
-int main() {
-    int c;
-    int p;
-    int len=0;
-    int d=0;
-    scanf("%d" , &p);
-    if (!(p == -1)){
-        len = 1; 
-        d = 1;
-        scanf("%d" , &c);
-        while(!(c==-1)){
-            if (p < c){
-                len = len + 1;
-
-            }
-            else{
-                if (len > d){
-                    d = len;
-                }
-                len = 1;
-            }
-            p = c;
-            scanf("%d" , &c);
-            if (len > d){
-                d = len;
-            }
-        }
-        
-    }
-    printf("%d\n" , d);
-}
+/*
+ * This program reads a sequence of integers from the user, terminated by -1,
+ * and finds the length of the longest subsequence of consecutive numbers
+ * that are strictly increasing.
+ *
+ * Variables:
+ * - p: holds the previous number in the sequence.
+ * - c: holds the current number in the sequence.
+ * - len: tracks the length of the current increasing sequence.
+ * - d: stores the maximum length (i.e., the longest increasing sequence found).
+ *
+ * Steps:
+ * 1. Read the first number (p).
+ * 2. If the first number is not -1 (the termination value), initialize len and d to 1.
+ * 3. Read the next number (c) and enter a loop that continues until c equals -1.
+ * 4. Inside the loop:
+ *    - If the previous number (p) is less than the current number (c), increment len.
+ *    - Otherwise (i.e., the sequence is not increasing), update d if len is larger than d,
+ *      and then reset len to 1.
+ *    - Update p to be the current number (c) and read the next number into c.
+ *    - After reading, update d if the current len is greater than d.
+ * 5. Once the loop terminates, print the maximum length (d) found.
+ */
